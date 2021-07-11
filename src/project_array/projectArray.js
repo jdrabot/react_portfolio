3056fa8f132480f1deaf4b35698f405114d3289b
jdrabot <jdrabot@hotmail.com>
2021-07-11 12:26:15 -0400

    created a projects array to pass in all projects

---
+++ src/project_array/projectArray.js
@@ -0,0 +1,45 @@
+import castle from "../images/castle.png";
+import fitnessTracker from "../images/fitness_tracker.jpeg";
+import mediaFi from "../images/media-fi.jpeg";
+import beerBox from "../images/screenshot_of_ecommerce_beerbox.png";
+import teamProfileGenerator from "../images/team_profile_generator.jpeg";
+
+const projects = [
+    {
+        "title": "Castle of Madness",
+        "description": "Old-school text adventure game. Was worked on in a group.",
+        "image": castle,
+        "website": "https://github.com/jdrabot/text-adventure-game",
+        "repository": "https://github.com/jdrabot/text-adventure-game"
+    },
+    {
+        "title": "Fitness Tracker",
+        "description": "An app that allows you to track your fitness and add workouts.",
+        "image": fitnessTracker,
+        "website": "https://jdrabot.github.io/workout_tracker/",
+        "repository": "https://github.com/jdrabot/workout_tracker"
+    },
+    {
+        "title": "Media-Fi",
+        "description": "An app that allows you to curate different songs based on a gif you choose. Was worked on with a group of students.",
+        "image": mediaFi,
+        "website": "https://polar-atoll-49785.herokuapp.com/index.html",
+        "repository": "https://github.com/jdrabot/Media-fi/tree/main"
+    },
+    {
+        "title": "Beer Box",
+        "description": "",
+        "image": beerBox,
+        "website": "https://ecomercebeerbox.herokuapp.com/",
+        "repository": "https://github.com/jdrabot/eComerce_Beer_Box/tree/main"
+    },
+    {
+        "title": "Team Profile Generator",
+        "description": "From the command line, it allows you to create your team from prompts served to you.",
+        "image": teamProfileGenerator,
+        "website": "https://github.com/jdrabot/Team_Profile_Generator",
+        "repository": "https://github.com/jdrabot/Team_Profile_Generator"
+    },
+]
+
+export default projects;
